06dfe35496c93b60bfdfe8c009c40f06d09e3f3b
henriquesagawauem <ra152725@uem.br>
2026-04-07 22:14:55 -0300

    Add files via upload

---
+++ DMA para AMD/main.py
@@ -0,0 +1,13 @@
+def dma_para_amd(data: str) -> str:
+    """
+    Transforma *data*, que deve estar no formato "dia/mes/ano",
+    onde dia e mes tem dois dígitos e ano tem quatro dígitos,
+    para o formato "ano/mes/dia".
+
+    Exemplos
+    >>> dma_para_amd("15/05/2005")
+    '2005/05/15'
+    >>> dma_para_amd("26/08/1914")
+    '1914/08/26'
+    """
+    return f"{data[6:]}/{data[3:5]}/{data[:2]}"
