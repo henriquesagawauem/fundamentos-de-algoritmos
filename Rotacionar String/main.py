c4ac16862d8e91aecbf9ee0a09de443553261d71
Henrique Tutomu Sagawa <ra152725@uem.br>
2026-04-09 15:26:30 -0300

    Adicionando "análise" e "tipos de dados" no código

--- Rotacionar String/main.py
+++ Rotacionar String/main.py
@@ -1,6 +1,14 @@
+# Análise
+# Rotacionar um string. Uma rotação de string se por mover os últimos
+# n caracteres de um string para as n primeiras posições da string
+#
+# Tipos de dados
+# uma palavra qualquer com pelo menos 1 caractere
+
+
 def rotacionar_string(string: str, n: int) -> str:
     """
-    Rotaciona uma string n posições a direita. Isto é, significa mover os últimos
+    Rotaciona uma string n posições a direita, movendo os últimos
     n caracteres de uma string para as n primeiras posições da string
 
     Exemplos
@@ -8,5 +16,7 @@ def rotacionar_string(string: str, n: int) -> str:
     'celiomar'
     >>> rotacionar_string("fernando", 3)
     'ndoferna'
+    >>> rotacionar_string("a", 4)
+    'a'
     """
     return f"{string[-n:]}{string[0:-n]}"
