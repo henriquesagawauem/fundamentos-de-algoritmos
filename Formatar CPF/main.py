1e1e945e28431ee059d9a82bec8563c3d807e9c7
Henrique Tutomu Sagawa <ra152725@uem.br>
2026-04-09 20:01:51 -0300

    Atualização projeto de função completo

--- Formatar CPF/main.py
+++ Formatar CPF/main.py
@@ -1,11 +1,38 @@
-def format_cpf(cpf: str) -> str:
+# Análise
+# Receber um CPF sem formatação e formtá-lo para o padrão xxx.xxx.xxx-xx
+#
+# Tipos de dados
+# CPF do usuário, onde o CPF não tem formtação
+
+
+def formatar_cpf(cpf: str) -> str:
     """
-    Formata o cpf de um usuário para ser representado com a formatação padrão
-    xxx.xxx.xxx-xx. Portanto, recebe o cpf sem formatação (12345678910) e retorna
-    formado padrão (123.456.789-10)
+    Receber o cpf do usuário sem formatação, no formato de string
+    e retorná-lo formatado no padrão xxx.xxx.xxx-xx
 
     Exemplos
-    >>> format_cpf('12345678912')
-    '123.456.789-12'
+    >>> formatar_cpf("12345678901")
+    '123.456.789-01'
+    >>> formatar_cpf("98765432110")
+    '987.654.321-10'
     """
     return f"{cpf[0:3]}.{cpf[3:6]}.{cpf[6:9]}-{cpf[9:11]}"
+
+
+# Análise
+# Receber um cpf com formatação xxx.xxx.xxx-xx e remover sua formatação para xxxxxxxxxxx
+#
+# Tipos de dados
+# CPF do usuário, onde o CPF está formatado em xxx.xxx.xxx-xx
+
+
+def desformatar_cpf(cpf: str) -> str:
+    """
+    Recebero cpf do usuário com formatação, no formato de string
+    e retorná-lo sem formatação
+
+    Exemplos
+    >>> desformatar_cpf("123.456.789-01")
+    '12345678901'
+    """
+    return f"{cpf[0:3]}{cpf[4:7]}{cpf[8:11]}{cpf[12:14]}"
