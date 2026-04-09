de4034f0692170e232c0adae261c6d985bb9fbbb
Henrique Tutomu Sagawa <ra152725@uem.br>
2026-04-09 15:23:17 -0300

    Adicionando "análise" e "tipos de dados" no código

--- Número Digitos/main.py
+++ Número Digitos/main.py
@@ -1,3 +1,10 @@
+# Análise
+# Contar o número de digitos de um determinado número inteiro
+#
+# Tipo de dados
+# Um número inteiro, podendo ser tanto negativo quanto positivo
+#
+#
 def numero_digitos(n: int) -> int:
     """
     Retorna o número de caracteres contidos em *n*. Para números negativas,
